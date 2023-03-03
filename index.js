6f8f7eef1c7bd65582426445fd99a718fa057db9
Miguel-Barros-Clear <miguel.barros.clear@gmail.com>
2023-03-03 12:05:00 -0300

    perf: improving the way to collect form data

--- index.js
+++ index.js
@@ -7,3 +7,14 @@ var password = document.querySelector("#exampleInputPassword");
 var photo = document.querySelector("#exampleInputFile");
 var admin = document.querySelector("#exampleInputAdmin");
 
+var fields = document.querySelectorAll("#form-user-create [name]");
+
+fields.forEach((field, index) => {
+    if (field.name == "gender") {
+        if (field.checked) { }
+    } else {
+        return
+    }
+
+    console.log(field.name);
+});
