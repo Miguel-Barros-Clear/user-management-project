9f6b59da81ee73f6a3fd9e35e47f135269125575
Miguel-Barros-Clear <miguel.barros.clear@gmail.com>
2023-03-03 12:13:18 -0300

    feat: adding user variable

--- index.js
+++ index.js
@@ -1,20 +1,14 @@
-var nome = document.querySelector("#exampleInputName");
-var gender = document.querySelector("#form-user-create [name=gender]:checked");
-var birth = document.querySelector("#exampleInputBirth");
-var country = document.querySelector("#exampleInputCountry");
-var email = document.querySelector("#exampleInputEmail");
-var password = document.querySelector("#exampleInputPassword");
-var photo = document.querySelector("#exampleInputFile");
-var admin = document.querySelector("#exampleInputAdmin");
-
 var fields = document.querySelectorAll("#form-user-create [name]");
+var user = {};
 
 fields.forEach((field, index) => {
     if (field.name == "gender") {
-        if (field.checked) { }
+        if (field.checked) {
+            user[field.name] = field.value;
+        }
     } else {
-        return
+        user[field.name] = field.value;
     }
-
-    console.log(field.name);
 });
+
+console.log(user);
