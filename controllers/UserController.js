e977cb2a01f6859d80a9feda8ea9cfb2e39a7318
Miguel-Barros-Clear <miguel.barros.clear@gmail.com>
2023-03-06 12:18:59 -0300

    feat: delete user function

--- controllers/UserController.js
+++ controllers/UserController.js
@@ -155,7 +155,7 @@ class UserController {
             <td>${Utils.dateFormat(dataUser.register)}</td>
             <td>
                 <button type="button" class="btn btn-primary btn-edit btn-xs btn-flat">Editar</button>
-                <button type="button" class="btn btn-danger btn-xs btn-flat">Excluir</button>
+                <button type="button" class="btn btn-danger btn-delete btn-xs btn-flat">Excluir</button>
             </td>
         `
 
@@ -166,6 +166,15 @@ class UserController {
     }
 
     addEventsTR(tr) {
+        tr.querySelector(".btn-delete").addEventListener("click", (e) => {
+
+            if (confirm("Deseja realmente excluir?")) {
+                tr.remove();
+                this.updateCount();
+            }
+
+        });
+
         tr.querySelector(".btn-edit").addEventListener("click", (e) => {
             let json = JSON.parse(tr.dataset.user);
             this.formUpdateEl.dataset.trIndex = tr.sectionRowIndex;
