2ec75857f3d438ebc782e79dac8822546cf054fd
Miguel-Barros-Clear <miguel.barros.clear@gmail.com>
2023-03-06 09:31:54 -0300

    feat: adding date and utils class

--- controllers/UserController.js
+++ controllers/UserController.js
@@ -8,11 +8,15 @@ class UserController {
     onSubmit() {
         this.formEl.addEventListener('submit', (e) => {
             e.preventDefault();
+            let btn = this.formEl.querySelector("[type=submit]");
+            btn.disabled = true;
             let values = this.getValues();
 
             this.getPhoto().then((content) => {
                 values.photo = content;
                 this.addLine(values);
+                this.formEl.reset();
+                btn.disabled = false;
             }, (err) => {
                 console.log(err);
             })
@@ -38,7 +42,12 @@ class UserController {
                 reject(e);
             }
 
-            fileReader.readAsDataURL(file);
+            if (file) {
+                fileReader.readAsDataURL(file);
+            } else {
+                resolve('dist/img/boxed-bg.jpg');
+            }
+
         })
     }
 
@@ -50,6 +59,8 @@ class UserController {
                 if (field.checked) {
                     user[field.name] = field.value;
                 }
+            } else if (field.name == "admin") {
+                user[field.name] = field.checked;
             } else {
                 user[field.name] = field.value;
             }
@@ -68,18 +79,21 @@ class UserController {
     }
 
     addLine(dataUser) {
-        this.tableEl.innerHTML = `
-        <tr>
+
+        let tr = document.createElement("tr");
+
+        tr.innerHTML = `
             <td><img src="${dataUser.photo}" alt="User Image" class="img-circle img-sm"></td>
             <td>${dataUser.name}</td>
             <td>${dataUser.email}</td>
-            <td>${dataUser.admin}</td>
-            <td>${dataUser.birth}</td>
+            <td>${(dataUser.admin) ? "Sim" : "Não"}</td>
+            <td>${Utils.dateFormat(dataUser.register)}</td>
             <td>
                 <button type="button" class="btn btn-primary btn-xs btn-flat">Editar</button>
                 <button type="button" class="btn btn-danger btn-xs btn-flat">Excluir</button>
             </td>
-        </tr>
         `
+
+        this.tableEl.appendChild(tr);
     }
 }
